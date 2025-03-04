0fb64b59b855df569a226a33eafe910e9d91e491
GabrielFernadess <gsf201919@gmail.com>
2025-03-04 14:19:37 +0000

    // dois cógdigos, o primeiro com base no que eu havia entendido e o segundo contou com base em pesquisas na internet de como aprimorar meu código e como implementar  linhas de códigos que o deixassem mais fácil de entender e de ser executado, li e reli cada parte e estudei o conteúdo que não havia entendido

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -1,12 +1,8 @@
 #include <stdio.h>
 
-// Desafio Super Trunfo - Países
-// Tema 1 - Cadastro das Cartas
-// Este código inicial serve como base para o desenvolvimento do sistema de cadastro de cartas de cidades.
-// Siga os comentários para implementar cada parte do desafio.
-//Teste larissa
 
-int main() {
+//código feito com base somente em meus conhecimento e no que aprendi
+
     int main(){
         char estado[50];
         char cidade[50];
@@ -95,6 +91,6 @@ int main() {
  
         return 0;
  
-     }
+     
      
 }
